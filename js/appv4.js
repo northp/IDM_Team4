29950adaad229625f6c6b31e261a5b8184b421de
Linh Nguyen <ngyn@Linhs-MacBook-Pro.local>
2019-07-30 15:44:48 +0100

    show dangerzone when answer is correct (not dynamic yet)

--- js/appv4.js
+++ js/appv4.js
@@ -1503,18 +1503,30 @@ function dangerArea(planet) {
             metalBool = true;
         }
         yPosition = 0;
-
-
     }
 
+ 
+    $("#submit").click(function(){
+        if ($('input[type=radio]:checked').val() == 0){
+            console.log($('input[type=radio]:checked').val());
+            drawZone();
+            $(".modal").hide();
+            $("label").remove();
+            $(".right img").remove();
+
+        } else if ( $('input[type=radio]:checked').val() != 0){
+            console.log($('input[type=radio]:checked').val());
+        }
+      
+    });
+
     //when the user clicks on <span> (x)
-    span.onclick = function () {
-        drawZone();
+/*     span.onclick = function () {
+        //drawZone();
         for (var x = 0; x < 6; x++) {
             $(".gameImg").attr("src", "");
             $("." + x).remove();
             $("label").remove();
-            $("#submit").remove();
         }
 
     };
@@ -1522,15 +1534,14 @@ function dangerArea(planet) {
     //when the user clicks anywhere outside of the modal
     window.onclick = function (event) {
         if (event.target == modal) {
-            drawZone();
+            //drawZone();
             for (var x = 0; x < 6; x++) {
                 $(".gameImg").attr("src", "");
                 $("." + x).remove();
                 $("label").remove();
-                $("#submit").remove();
             }
         }
-    };
+    }; */
 
 }
 
@@ -1578,7 +1589,11 @@ function clickElements() {
         $commandsOverlay.hide();
     });
 
+
+
+
     $planetFire.click(function () {
+        var x = 0;
         modal.style.display = "block";
         $modalImage.attr("src", "img/playfield/planets/planet_fire.png").css("height", "3%", "width", "3%");
         $modalText.text("Choose the right answer to solve the puzzle.");
@@ -1590,14 +1605,13 @@ function clickElements() {
         $commandsOverlay.hide();
         dangerArea("fire");
 
-        for (var x = 0; x < 6; x++) {
-            $(".right").append("<label><input type='radio' name='answer' value=' " + x + "'><img class='" + x + "'></label>");
+        for (x; x < 6; x++) {
+            $(".labels").append("<label><input type='radio' name='answer' value=' " + x + "'><img class='" + x + "'></label>");
             $("." + x).attr("src", "img/minigames/minigame_one_" + x + ".png");
             $("." + x).css("width", "30%");
             $("." + x).css("margin-right", "2%");
+            $("#submit").show();
         }
-        $(".right").append("<input type='submit' value='submit' id='submit'>");
-
     });
 
     $planetMetal.click(function () {
@@ -1665,6 +1679,11 @@ function clickElements() {
             modal.style.display = "none";
             $point.hide();
             $commandsOverlay.hide();
+            for (var x = 0; x < 6; x++) {
+                $(".gameImg").attr("src", "");
+                $("." + x).remove();
+                $("label").remove();
+            }
         } else if (event.target == navModal) {
             navModal.style.display = "none";
             $point.hide();
@@ -1680,6 +1699,11 @@ function clickElements() {
         navModal.style.display = "none";
         $point.hide();
         $commandsOverlay.hide();
+        for (var x = 0; x < 6; x++) {
+            $(".gameImg").attr("src", "");
+            $("." + x).remove();
+            $("label").remove();
+        }
     };
 }
 
