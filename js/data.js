eb2cc1e816abbbe5d151d2a717d501e72e9e366e
northp <northp@tcd.ie>
2019-08-07 11:23:44 +0100

    Adding JSON file, node entry

---
+++ js/data.js
@@ -0,0 +1,73 @@
+'use strict';
+
+var fs = require('fs');
+
+// A function to find the current Level (tracked in JSON file)
+function findCurrentLevel(){
+    fs.readFile("index.json", (err, results) => {
+        if (err) {
+            return console.error(err);
+        };
+        var results = JSON.parse(results.toString());
+        console.log("Current Level: " + results.current_level);
+    });
+}
+
+// A function to find the highest completed Level (tracked in JSON file)
+function findHighestCompletedLevel(){
+    fs.readFile("index.json", (err, results) => {
+        if (err) {
+            return console.error(err);
+        };
+        var results = JSON.parse(results.toString());
+        console.log("Highest Completed Level: " + results.highest_level_completed);
+    });
+}
+
+// a function to reset the current level to 0 (can be modified, in theory, to choose any previously completed level)
+function resetProgress(){
+    fs.readFile("index.json", (err, results) => {
+        if(err){
+            return console.log(err);
+        }
+        var results = JSON.parse(results.toString());
+        results.current_level = 1;
+        var currentLevel = results.current_level;
+        var writeData = fs.writeFile("index.json", JSON.stringify(results), (err) => {
+            if (err) {
+                return console.error(err);
+            } else {
+                console.log("Current Level: " + results.current_level);
+                console.log("Highest Level Completed: " + results.highest_level_completed);
+            }
+        });
+        response.send("Reset current Level to 1; current Level: " + results.current_level + "; Highest Level Completed: " + results.highest_level_completed);
+        
+    });
+}
+
+// a function to "complete" a level. Should increment current level. Should only increment highest level completed if current level completed is higher than highest level completed in the JSON file.
+function completeLevel(){
+    fs.readFile("index.json", (err, results) => {
+        if (err) {
+            return console.error(err);
+        };
+        var results = JSON.parse(results.toString());
+        var currentLevel = results.current_level;
+        if (results.highest_level_completed > currentLevel) {
+            results.highest_level_completed = results.highest_level_completed;
+            results.current_level = currentLevel + 1;
+        } else if (results.highest_level_completed <= currentLevel) {
+            results.highest_level_completed = currentLevel;
+            results.current_level = currentLevel + 1;
+        }
+        var writeData = fs.writeFile("index.json", JSON.stringify(results), (err) => {
+            if (err) {
+                return console.error(err);
+            } else {
+                console.log("Current Level: " + results.current_level);
+                console.log("Highest Level Completed: " + results.highest_level_completed);
+            }
+        });
+    });
+}
